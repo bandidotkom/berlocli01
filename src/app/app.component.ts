b44146b848db68300826c8968bff46bbec47003a
bandidotkom <andras.komaromy@fu-berlin.de>
2018-04-08 13:31:47 +0200

    add server

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { tileLayer, latLng} from "leaflet";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = "BERLO";
+}
