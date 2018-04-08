cbf2ff7092e98ee06d7a3ffd2ff65d1e28a07904
bandidotkom <andras.komaromy@fu-berlin.de>
2018-04-08 18:59:23 +0200

    added compareListExplore

--- ang-src/src/app/explore/explore.component.ts
+++ ang-src/src/app/explore/explore.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
   selector: 'app-explore',
@@ -6,14 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./explore.component.css']
 })
 export class ExploreComponent implements OnInit {
-  selected: String;
+  selected: string;
   constructor() { }
-
+  compareListExplore = [];
   ngOnInit() {
   }
 
-  onSelected(localty: String) {
+  onSelected(localty: string) {
     this.selected = localty;
+  }
+  addToCompare() {
+    let i = this.compareListExplore.length;
+    if (this.compareListExplore.includes(this.selected)) {
+      alert(this.selected + " was already added.");
+    }
+    else if (i < 5) {
+      this.compareListExplore[i] = this.selected;
+      console.log(this.compareListExplore);
+    }
+    else {
+      alert("You can compare only 5 localties at maximum.");
+    }
   }
 
 }
